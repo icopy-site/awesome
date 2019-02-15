f71436b7f43963e886654b9360736b2593469189
chenjiajia <jiajia.chen@dewmobile.net>
2019-02-15 12:22:13 +0800

    回滚配置文件修改

--- custom_theme.sh
+++ custom_theme.sh
@@ -14,10 +14,10 @@ if [ ! -d "mkdocs-material" ] ; then
   git clone --depth 1 https://github.com/squidfunk/mkdocs-material.git
 fi
 sed -i "s/name: 'material'/name: null\n  custom_dir: 'mkdocs-material\/material'\n  static_templates:\n    - 404.html/g" mkdocs.yml
-
 sed -i "s/<head>/<head>\n <script>base_url='{{ base_url }}'<\/script>/g" mkdocs-material/material/base.html
 
 # Change Google CDN to loli.net
 sed -i 's/fonts.gstatic.com/gstatic.loli.net/g' mkdocs-material/material/base.html
 sed -i 's/fonts.googleapis.com/fonts.loli.net/g' mkdocs-material/material/base.html
 cp ./static/gitalk.html mkdocs-material/material/partials/integrations/disqus.html
+
